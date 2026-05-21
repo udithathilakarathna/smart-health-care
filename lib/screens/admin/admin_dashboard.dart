cbe2958b4dd0a9448cf44ce833cca60d35f51758
Uditha Thilakarathana <157568367+udithathilakarathna@users.noreply.github.com>
2026-05-21 12:23:22 +0530

    Added admin logout functionality

--- lib/screens/admin/admin_dashboard.dart
+++ lib/screens/admin/admin_dashboard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'doctor_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../auth/login_screen.dart';
 
 class AdminDashboard extends StatelessWidget {
   const AdminDashboard({super.key});
@@ -89,14 +91,14 @@ class AdminDashboard extends StatelessWidget {
               icon: Icons.local_hospital,
               title: "Doctors",
               onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) =>
-                    const DoctorsScreen(),
-                  ),
-                );
-              },
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) =>
+                  const DoctorsScreen(),
+                ),
+              );
+            },
             ),
 
             buildDrawerItem(
@@ -130,8 +132,78 @@ class AdminDashboard extends StatelessWidget {
             buildDrawerItem(
               icon: Icons.logout,
               title: "Logout",
-              onTap: () {},
               color: Colors.red,
+              onTap: () async {
+
+                bool? confirmLogout =
+                await showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      shape:
+                      RoundedRectangleBorder(
+                        borderRadius:
+                        BorderRadius.circular(
+                            20),
+                      ),
+                      title:
+                      const Text("Logout"),
+                      content: const Text(
+                        "Are you sure you want to logout?",
+                      ),
+                      actions: [
+
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(
+                                context,
+                                false);
+                          },
+                          child:
+                          const Text("Cancel"),
+                        ),
+
+                        ElevatedButton(
+                          style:
+                          ElevatedButton.styleFrom(
+                            backgroundColor:
+                            Colors.red,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(
+                                context,
+                                true);
+                          },
+                          child:
+                          const Text(
+                            "Logout",
+                            style: TextStyle(
+                                color:
+                                Colors.white),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
+
+                if (confirmLogout ==
+                    true) {
+
+                  await FirebaseAuth
+                      .instance
+                      .signOut();
+
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) =>
+                      const LoginScreen(),
+                    ),
+                        (route) => false,
+                  );
+                }
+              },
             ),
           ],
         ),
