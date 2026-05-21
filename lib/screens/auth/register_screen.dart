3384a28ddc6fd66c199d59015d6236d5a9665509
dhanushka9987 <ndanushka16889@email.com>
2026-05-21 14:15:46 +0530

    Added staff management system

--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -158,7 +158,7 @@ class _RegisterScreenState
         boxShadow: [
           BoxShadow(
             color: Colors.grey
-                .withOpacity(0.08),
+                .withValues(alpha: 0.08),
             blurRadius: 12,
             spreadRadius: 2,
           )
