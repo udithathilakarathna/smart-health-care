6b04f9df44cbecfa8cf4453e638821312ed4e8b0
Uditha Thilakarathana <157568367+udithathilakarathna@users.noreply.github.com>
2026-06-19 12:13:24 +0530

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Firebase Demo',
+      title: 'Smart Care',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
